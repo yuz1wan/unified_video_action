50c52e6e9f0b950f10ff44275a0846657ef322a5
Wang Ziyu <1172789766@qq.com>
2025-03-17 22:48:49 +0800

    [feat]适配human hand视频生成任务

--- train_human_pp.sh
+++ train_human_pp.sh
@@ -1,8 +1,8 @@
-CUDA_VISIBLE_DEVICES=2,3 accelerate launch --num_processes=2 train.py \
+CUDA_VISIBLE_DEVICES=1,2,3 accelerate launch --num_processes=3 train.py \
     --config-dir=. \
-    --config-name=uva_pusht.yaml \
+    --config-name=uva_human_pp.yaml \
     model.policy.action_model_params.predict_action=False \
     model.policy.selected_training_mode=video_model \
     model.policy.optimizer.learning_rate=1e-4 \
     logging.project=uva \
-    hydra.run.dir="checkpoints/uva_pusht_video_model"
+    hydra.run.dir="checkpoints/uva_human_pp_video_model"
